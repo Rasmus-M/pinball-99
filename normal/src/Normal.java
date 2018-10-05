b682466000ac0cceb17546957dcfe1438068361e
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-10-05 18:30:31 +0200

    Sprite pattern and normal table

--- normal/src/Normal.java
+++ normal/src/Normal.java
@@ -1,2 +1,26 @@
 public class Normal {
+
+    public static void main(String[] args) {
+
+        for (int angle = 0; angle < 255; angle++) {
+            Vec2 normal = new Vec2(0, -50);
+            double a = (2 * Math.PI) * (angle / 255d);
+            normal.rotate(-a);
+            normal.normalize();
+            System.out.println("       data " + hex(normal.x) + "," + hex(normal.y));
+        }
+    }
+
+    private static String hex(double d) {
+        int i = (int) Math.round(d * 0x1000);
+        String hex = Integer.toHexString(i);
+        if (hex.length() > 4) {
+            hex = hex.substring(hex.length() - 4);
+        } else {
+            while (hex.length() < 4) {
+                hex = "0" + hex;
+            }
+        }
+        return ">" + hex;
+    }
 }
