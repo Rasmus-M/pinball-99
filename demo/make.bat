302ff6acab319eda922cb00d7f9acaa64191ae47
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-10-05 21:10:33 +0200

    Build 16k

--- demo/make.bat
+++ demo/make.bat
@@ -8,8 +8,6 @@ xas99.py -R -i -o PINBALL source/pinball.a99
 
 xdm99.py pinball.dsk -a pinball -n PBDEMO
 
-java -jar tools/ea5tocart.jar pinball "PINBALL DEMO" > make.log
-
-tools\pad.exe pinball8.bin pinball-16k.bin 16384
-copy /b pinball-16k.bin + coll.bin pinball-demo-8.bin
+java -jar tools/ea5tocart.jar pinball "PINBALL DEMO" 0 16k > make.log
+copy /b pinball8.bin + coll.bin pinball-demo-8.bin
 
