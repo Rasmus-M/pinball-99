8a59252ab298d4bd7e46d28f8329e726c5c86c08
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2022-03-04 18:19:00 +0100

    Improved map

--- demo2/make.bat
+++ demo2/make.bat
@@ -18,4 +18,4 @@ copy /b pinball8.bin + ^
     empty.bin ^
     pinball-demo2-8.bin
 
-rem java -jar tools/CopyHeader.jar pinball-demo2-8.bin
+java -jar tools/CopyHeader.jar pinball-demo2-8.bin 60 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
