b3d9109869282fe78204bd0c1418a1a1fed55603
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2022-03-26 19:25:10 +0100

    Top panel

--- make.bat
+++ make.bat
@@ -13,4 +13,4 @@ copy /b bin\pinball8.bin + ^
     bin\empty.bin ^
     pinball-8.bin
 
-java -jar tools/CopyHeader.jar pinball-8.bin 60 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+java -jar tools/CopyHeader.jar pinball-8.bin 60 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
