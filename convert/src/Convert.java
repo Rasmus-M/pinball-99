cefb9ee12ef076d4cf67154f4b1a719aa6a724b9
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2022-03-30 21:52:51 +0200

    Special

--- convert/src/Convert.java
+++ convert/src/Convert.java
@@ -3,13 +3,16 @@ import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Convert {
 
     public static void main(String[] args) throws IOException {
+        new Convert().run();
+    }
+
+    private void run() throws IOException {
         JFileChooser fileChooser = new JFileChooser(".");
         fileChooser.setFileFilter(new FileFilter() {
             @Override
@@ -24,31 +27,63 @@ public class Convert {
         int result = fileChooser.showOpenDialog(null);
         if (result == JOptionPane.YES_OPTION) {
             File file = fileChooser.getSelectedFile();
-            BufferedImage image = ImageIO.read(file);
-            byte[] raw = new byte[image.getHeight() * image.getWidth()];
-            int i = 0;
-            for (int y = 0; y < image.getHeight(); y++) {
-                for (int x = 0; x < image.getWidth(); x++) {
-                    int rgb = image.getRGB(x, y);
-                    // Add 1 to wrap around because 255 is empty background
-                    raw[i++] = (byte) (((rgb >> 16) + 1) & 0xff);
-                }
-            }
+            byte[] raw = readFile(file);
+            File specialFile = new File("./" + getBaseFilename(file) + "-special.png");
+            byte[] special = specialFile.exists() ? readSpecialFile(specialFile) : null;
             int bankSize = 0x2000;
             int headerSize = 0x0000;
             int dataSize = 0x2000;
             int banks = (raw.length / dataSize) + (raw.length % dataSize == 0 ? 0 : 1);
             byte[] rom = new byte[banks * bankSize];
-            i = 0;
+            int i = 0;
             for (int b = 0; b < banks; b++) {
                 for (int j = 0; j < dataSize; j++) {
-                    rom[b * bankSize + headerSize + j] = i < raw.length ? raw[i++] : 0;
+                    byte value = i < raw.length ? (byte) (raw[i] | (special != null ? special[i] : 0)) : 0;
+                    rom[b * bankSize + headerSize + j] = value;
+                    i++;
                 }
             }
-            String outputFilename = "..\\bin\\" + file.getName().replaceAll("\\.(png|gif)", ".bin");
+            String outputFilename = "..\\bin\\" + getBaseFilename(file) + ".bin";
             FileOutputStream fos = new FileOutputStream(outputFilename);
             fos.write(rom);
             fos.close();
         }
+    }
+
+    private String getBaseFilename(File file) {
+        return file.getName().split("\\.")[0];
+    }
+
+    private byte[] readFile(File file) throws IOException {
+        BufferedImage image = ImageIO.read(file);
+        byte[] raw = new byte[image.getHeight() * image.getWidth()];
+        int i = 0;
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                int rgb = image.getRGB(x, y);
+                int red = (rgb >> 16) & 0xff; // 0, 2, 4, 6 ... 254, 255 = empty
+                int value = red == 255 ? 0 : (red / 2) + 1;
+                if (value == 128) {
+                    value = 127;
+                }
+                raw[i++] = (byte) value;
+            }
+        }
+        return raw;
+    }
+
+    private byte[] readSpecialFile(File file) throws IOException {
+        BufferedImage image = ImageIO.read(file);
+        byte[] raw = new byte[image.getHeight() * image.getWidth()];
+        int i = 0;
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                int rgb = image.getRGB(x, y);
+                int red = (rgb >> 16) & 0xff;
+                int value = red != 0 ? 128 : 0;
+                raw[i++] = (byte) value;
+            }
+        }
+        return raw;
     }
 }
