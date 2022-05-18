43956f2e013829ca7dbbcf102de0b47aa2fcdbe7
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2022-05-18 21:50:40 +0200

    Music

--- sound/convert-music.bat
+++ sound/convert-music.bat
@@ -1,2 +1,7 @@
 java -jar ..\tools\epsgmod.jar tune1
 copy tune1-music.a99 ..\source\
+del tune1-music.a99
+
+java -jar ..\tools\epsgmod.jar tune2
+copy tune2-music.a99 ..\source\
+del tune2-music.a99
